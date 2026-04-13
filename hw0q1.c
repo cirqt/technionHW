fc738f0ea75e823f99ec8e5113a130f113a744b6
cirqt <Circulating1337@gmail.com>
2026-04-13 15:18:01 +0300

    homeword week 0

---
+++ hw0q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+    for(int i = 3; i > 0; i--) {
+        printf("%d\n", i);
+    }
+    printf("Hello, World!\n");
+    /*
+     * Well,
+     *Are you ready?
+     */
+    return 0;
+}
