88d64e95378afa18da57348fe9e2b2708d803fe4
cirqt <Circulating1337@gmail.com>
2026-04-30 18:56:27 +0300

    added loop till eof

--- hw1q1a.c
+++ hw1q1a.c
@@ -2,14 +2,15 @@
 
 int main() {
     int input = 0;
-    scanf("%d", &input);
-    if (input >= 100 && input <= 999)
-        printf("Triple Digit.");
-    else if (input % 2 == 0 && input > 0)
-        printf("Positive Even.");
-    else if (input % 2 == -1 && input < 0)
-        printf("Negative Odd.");
-    else
-        printf("Other.");
+    while (scanf("%d", &input) != EOF) {
+        if (input >= 100 && input <= 999)
+            printf("Triple Digit.\n");
+        else if (input % 2 == 0 && input > 0)
+            printf("Positive Even.\n");
+        else if (input % 2 == -1 && input < 0)
+            printf("Negative Odd.\n");
+        else
+            printf("Other.\n");
+    }
 
 }
