8bd615b6c52807fdfe9628f937d43fe351a47b36
cirqt <Circulating1337@gmail.com>
2026-05-01 13:31:12 +0300

    format

--- hw1q1b.c
+++ hw1q1b.c
@@ -11,24 +11,24 @@ int main() {
         digitsOfN++; //example: n:12345 -> 5
         temp/=10;
     }
-    int firstPart = 0;
-    int multiplier = 1;
-    for (int i = digitsOfN - m; i > 0; i--) { //get the last n.length-m digits of n to remain
-        firstPart += n%10 * multiplier; //from n:12345 m:3 -> 5-3 last digits are 45
+    int firstPart = 0; //get the last n.length-m digits of n to remain
+    int multiplier = 1; //from n:12345 m:3 -> 5-3 last digits are 45
+    for (int i = digitsOfN - m; i > 0; i--) {
+        firstPart += n%10 * multiplier;
         multiplier *= 10;
         sumOfDigits += n%10;
         n /= 10;
     }
     int secondPart = 0;
-    while (m != 1) { //set up the multiplier such that the number will switch spots as intended
-        multiplier *= 10; //such the third digit will be multiplied by 10^x such that it will become the first digit
-        m--;
-    }
-    while (n != 0) { //multiply the digits by the corresponding multiplier to reverse them as intended
-        secondPart += n%10 * multiplier; //in the example case, 123 become 32100
-        multiplier /= 10;
-        sumOfDigits += n%10;
+    while (m != 1) { //set up the multiplier such that
+        multiplier *= 10; //the number will switch spots as intended
+        m--; //such the third digit will
+    } //be multiplied by 10^x such that it will become the first digit
+    while (n != 0) { //multiply the digits by
+        secondPart += n%10 * multiplier; //the corresponding
+        multiplier /= 10; //multiplier to reverse them as intended
+        sumOfDigits += n%10;//in the example case, 123 become 32100
         n /= 10;
     }
-    printf("Reversed: %d, Average: %f",firstPart+secondPart, sumOfDigits/digitsOfN); //add both numbers 32100 + 45 divide the sum of the digits by the number of digits
+    printf("Reversed: %d, Average: %.2f",firstPart+secondPart, sumOfDigits/digitsOfN); //add both numbers 32100 + 45 divide the sum of the digits by the number of digits
 }
