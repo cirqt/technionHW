5becc66c09ad6f78d2f0a5425df792d6a0a585ed
cirqt <Circulating1337@gmail.com>
2026-05-01 13:35:17 +0300

    format

--- hw1q1b.c
+++ hw1q1b.c
@@ -30,5 +30,7 @@ int main() {
         sumOfDigits += n%10;//in the example case, 123 become 32100
         n /= 10;
     }
-    printf("Reversed: %d, Average: %.2f",firstPart+secondPart, sumOfDigits/digitsOfN);
+    int sum = firstPart+secondPart;
+    double digitAverage = sumOfDigits/digitsOfN;
+    printf("Reversed: %d, Average: %.2f",sum, digitAverage);
 }//add both numbers 32100 + 45 divide the sum of the digits by the number of digits
