b67b40f4570533db54f0b936e4403580d51825e0
cirqt <Circulating1337@gmail.com>
2026-05-11 18:13:42 +0300

    fix handling for wrong input type

--- hw1q1c.c
+++ hw1q1c.c
@@ -2,37 +2,28 @@
 
 int main() {
     char c = 'a';
-    double k = 0;
-    scanf("%lf", &k);
-    scanf("%c", &c);
-    c++;
-    int roundK = (int)k;
-    if (roundK - k == 0) {
-        int letter = 0;
+    int k = 0;
+    if (scanf("%d %c", &k, &c) == 2) {
         if (c >= 'A' && c <= 'Z') {
-            c += 32;
-            letter = 1;
-        }
-        else if (c >= 'a' && c <= 'z') {
-            c -= 32;
-            letter = 1;
-
-        }
-        else
-            printf("Not A Letter.");
-        if (letter == 1) {
-            while (k != 0) {
-                if (c == 'z'+1) {
+            c += 33;
+            for (int i = 0; i < k; i++) {
+                if (c == 'z'+1)
                     c = 'a';
-                }
-                else if (c == 'Z'+1) {
-                    c = 'A';
-                }
-                k--;
-                printf("%c",c);
+                printf("%c", c);
                 c++;
             }
         }
+        else if (c >= 'a' && c < 'z') {
+            c -= 33;
+            for (int i = 0; i < k; i++) {
+                if (c == 'A'-1)
+                    c = 'Z';
+                printf("%c", c);
+                c--;
+            }
+        }
+        else
+            printf("Not A Letter.");
     }
     else
         printf("Wrong Input Type.");
