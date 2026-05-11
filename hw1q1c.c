d04b0da7365441e524c40f8854adfd34befa4cb2
cirqt <Circulating1337@gmail.com>
2026-05-11 18:16:08 +0300

    added comments

--- hw1q1c.c
+++ hw1q1c.c
@@ -3,20 +3,20 @@
 int main() {
     char c = 'a';
     int k = 0;
-    if (scanf("%d %c", &k, &c) == 2) {
-        if (c >= 'A' && c <= 'Z') {
+    if (scanf("%d %c", &k, &c) == 2) { //check input type matching
+        if (c >= 'A' && c <= 'Z') { //check uppercase
             c += 33;
-            for (int i = 0; i < k; i++) {
+            for (int i = 0; i < k; i++) { //iterate k times onwards from c
                 if (c == 'z'+1)
-                    c = 'a';
+                    c = 'a'; //if reaches the end go back to a
                 printf("%c", c);
                 c++;
             }
         }
-        else if (c >= 'a' && c < 'z') {
+        else if (c >= 'a' && c < 'z') { //check lowercase
             c -= 33;
-            for (int i = 0; i < k; i++) {
-                if (c == 'A'-1)
+            for (int i = 0; i < k; i++) { //iterate k times backwards from c
+                if (c == 'A'-1) //if reaches the end go back to Z
                     c = 'Z';
                 printf("%c", c);
                 c--;
